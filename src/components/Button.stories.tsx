a31fe0e0f0f0a3dd7a26dce2dc80262aa0760323
René Dudfield <renesd@gmail.com>
2020-11-21 20:57:08 +0100

    Clean up the Button.stories.tsx a bit.

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -20,20 +20,3 @@ export const Primary = Template.bind({});
 Primary.args = {
   color: "primary",
 };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
