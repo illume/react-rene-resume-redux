f734eb681755f4bf972cb86c4ddcaf1e38d2c8d8
René Dudfield <renesd@gmail.com>
2020-11-26 13:44:30 +0100

    Extract Copyright component from PageLayout.

---
+++ src/components/PageLayout/Copyright.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
+export default function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://renesd.blogspot.com/">
+        René Dudfield
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
