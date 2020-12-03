87550d8ff9beab365cf5c16923faf6cc0c990593
René Dudfield <renesd@gmail.com>
2020-12-03 18:44:48 +0100

    Put Experiences in a Grid.

--- src/features/experiences/Experiences.tsx
+++ src/features/experiences/Experiences.tsx
@@ -6,7 +6,7 @@ import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
-
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -29,9 +29,9 @@ const useStyles = makeStyles((theme: Theme) =>
       flexDirection: "column",
     },
     root: {
-      display: "flex",
-      padding: theme.spacing(2),
-      marginBottom: theme.spacing(2),
+      // display: "flex",
+      // padding: theme.spacing(2),
+      // marginBottom: theme.spacing(2),
     },
     tags: {
       display: "flex",
@@ -53,43 +53,45 @@ export function PureExperiences({ experiences }: PureExperiencesProps) {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
+    <Grid container spacing={3}>
       {experiences &&
         experiences.map((experience, index) => {
           return (
-            <Card
-              className={classes.root}
-              key={experience.title + experience.subtitle}
-            >
-              {experience.image && (
-                <CardMedia
-                  className={classes.cover}
-                  image={experience.image}
-                  title={experience.title}
-                />
-              )}
-              <div className={classes.details}>
-                <CardContent className={classes.content}>
-                  <Typography component="h5" variant="h5">
-                    {experience.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="textSecondary">
-                    {experience.subtitle}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="span"
-                  >
-                    <Markdown>{experience.body}</Markdown>
-                  </Typography>
-                  <Tags tags={experience.tags} />
-                </CardContent>
-              </div>
-            </Card>
+            <Grid item xs={12} md={6}>
+              <Card
+                className={classes.root}
+                key={experience.title + experience.subtitle}
+              >
+                {experience.image && (
+                  <CardMedia
+                    className={classes.cover}
+                    image={experience.image}
+                    title={experience.title}
+                  />
+                )}
+                <div className={classes.details}>
+                  <CardContent className={classes.content}>
+                    <Typography component="h5" variant="h5">
+                      {experience.title}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                      {experience.subtitle}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="span"
+                    >
+                      <Markdown>{experience.body}</Markdown>
+                    </Typography>
+                    <Tags tags={experience.tags} />
+                  </CardContent>
+                </div>
+              </Card>
+            </Grid>
           );
         })}
-    </React.Fragment>
+    </Grid>
   );
 }
 
